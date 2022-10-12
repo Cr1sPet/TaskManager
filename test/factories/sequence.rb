21bc21a464a7a20bb9418bf4ce650f8e84d7a50c
CrsPet <serezha.petrov.02@gmail.com>
2022-10-12 19:28:25 +0300

    edit sequence usage in users factory

--- test/factories/sequence.rb
+++ test/factories/sequence.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
-  sequence :string, aliases: [:first_name, :last_name, :password] do |n|
+  sequence :string, aliases: [:first_name, :last_name, :password, :avatar] do |n|
     "string#{n}"
   end
+
+  sequence :email do |n|
+    "person#{n}@example.com"
+  end
+
+
 end
