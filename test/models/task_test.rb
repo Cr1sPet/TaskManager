226d4a744416687d00032cf829ffc3f3a3f31f90
CrsPet <serezha.petrov.02@gmail.com>
2022-10-12 19:34:30 +0300

    add task factory test

--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 class TaskTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "create" do
+    task = create :task
+    assert task.persisted?
+  end
 end
