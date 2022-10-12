b66bd498d68385a763e57926a24974ce8093616d
CrsPet <serezha.petrov.02@gmail.com>
2022-10-12 21:58:55 +0300

    added relations between user and task

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
+
+
+  has_many :my_tasks, class_name: 'Task', foreign_key: :author_id
+  has_many :assigned_tasks, class_name: 'Task', foreign_key: :assignee_id
+
+
   has_secure_password
 end
