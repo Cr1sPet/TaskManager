dec1d737faa2c354a369cc15f2d70336106c5b0c
CrsPet <serezha.petrov.02@gmail.com>
2022-10-19 19:54:39 +0300

    added user authentication to boards controller

--- test/controllers/web/boards_controller_test.rb
+++ test/controllers/web/boards_controller_test.rb
@@ -5,4 +5,10 @@ class Web::BoardsControllerTest < ActionController::TestCase
     get :show
     assert_response :success
   end
+
+  setup do
+    user = create(:user)
+    sign_in user
+  end
+  
 end
