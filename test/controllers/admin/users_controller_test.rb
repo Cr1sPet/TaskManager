34f3462a515dfccb8ec1c580610f288a0a6c96e2
CrsPet <serezha.petrov.02@gmail.com>
2022-10-19 18:50:37 +0300

    added destroy user

--- test/controllers/admin/users_controller_test.rb
+++ test/controllers/admin/users_controller_test.rb
@@ -41,4 +41,10 @@ class Admin::UsersControllerTest < ActionController::TestCase
     assert_response :redirect
   end
 
+  test "should delete destroy" do
+    user = create(:user)
+    delete :destroy, params: {id: user.id }
+    assert_response :redirect
+  end
+
 end
