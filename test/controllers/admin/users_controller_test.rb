29b64e13dfd994e70bf7abc32e74af34c5d182b6
CrsPet <serezha.petrov.02@gmail.com>
2022-10-19 18:11:31 +0300

    added admin authorization

--- test/controllers/admin/users_controller_test.rb
+++ test/controllers/admin/users_controller_test.rb
@@ -1,9 +1,15 @@
 require 'test_helper'
 
 class Admin::UsersControllerTest < ActionController::TestCase
+  setup do
+    admin = create(:admin)
+    sign_in admin
+  end
+
   test "should get show" do
     user = create(:user)
     get :show, params: { id: user.id }
     assert_response :success
   end
+
 end
