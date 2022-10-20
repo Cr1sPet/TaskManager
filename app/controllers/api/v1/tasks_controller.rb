fe376c42ec97fb731c92df3c6d500183360fb8b1
CrsPet <serezha.petrov.02@gmail.com>
2022-10-20 18:31:49 +0300

    added tasks show endpoint

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -10,4 +10,10 @@ class Api::V1::TasksController < Api::V1::ApplicationController
     respond_with(tasks, each_serializer: TaskSerializer, root: 'items', meta: build_meta(tasks))
   end
 
+  def show
+    task = Task.find(params[:id])
+
+    respond_with(task, serializer: TaskSerializer)
+  end
+
 end
--- test/controllers/api/v1/tasks_controller_test.rb
+++ test/controllers/api/v1/tasks_controller_test.rb
@@ -4,7 +4,7 @@ class Api::V1::TasksControllerTest < ActionController::TestCase
 
 
   test "should get show" do
-    skip
+    
     author = create :user
     task = create :task, author: author
     get :show, params: { id: task.id, format: :json }
