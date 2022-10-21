5709eccaf0fc7a8baaa5acfb8fdaeef3a42879d4
CrsPet <serezha.petrov.02@gmail.com>
2022-10-21 21:48:48 +0300

    fixed error in webpack config

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,6 +1,5 @@
-const { environment } = require('@rails/webpacker')
-
-module.exports = environment
-
+const { environment } = require('@rails/webpacker');
 
 environment.loaders.delete('nodeModules');
+
+module.exports = environment;
