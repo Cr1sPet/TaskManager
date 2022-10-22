9958192735d9958c76cd594552c6d8f37eef13eb
CrsPet <serezha.petrov.02@gmail.com>
2022-10-22 12:18:45 +0300

    installed react-kanban and styled-components

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,10 @@ import Rails from '@rails/ujs';
 import * as ActiveStorage from '@rails/activestorage';
 import 'channels';
 import 'material-design-lite/material.js';
+import WebpackerReact from 'webpacker-react';
+import TaskBoard from './components/TaskBoard';
+
+WebpackerReact.setup({ TaskBoard });
 
 Rails.start();
 ActiveStorage.start();
