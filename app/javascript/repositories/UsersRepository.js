13a573f189d6da7241c7d542e1631eee28bdc987
CrsPet <serezha.petrov.02@gmail.com>
2022-10-30 18:43:34 +0300

    created UsersRepository

---
+++ app/javascript/repositories/UsersRepository.js
@@ -0,0 +1,14 @@
+import routes from 'routes';
+import FetchHelper from 'utils/fetchHelper';
+
+export default {
+  index(params) {
+    const path = routes.apiV1TasksPath();
+    return FetchHelper.get(path, params);
+  },
+
+  show(id) {
+    const path = routes.apiV1TaskPath(id);
+    return FetchHelper.get(path);
+  },
+};
