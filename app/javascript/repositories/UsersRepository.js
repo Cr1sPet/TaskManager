3b58d1866f0dbacdc515d32d9d3f646ed1944513
CrsPet <serezha.petrov.02@gmail.com>
2022-10-30 19:13:00 +0300

    fixed UsersRepository's pathes

--- app/javascript/repositories/UsersRepository.js
+++ app/javascript/repositories/UsersRepository.js
@@ -3,12 +3,12 @@ import FetchHelper from 'utils/fetchHelper';
 
 export default {
   index(params) {
-    const path = routes.apiV1TasksPath();
+    const path = routes.apiV1UsersPath();
     return FetchHelper.get(path, params);
   },
 
   show(id) {
-    const path = routes.apiV1TaskPath(id);
+    const path = routes.apiV1UserPath(id);
     return FetchHelper.get(path);
   },
 };
