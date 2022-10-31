d395d024b8710ebb9a77511549525256a67de5bf
CrsPet <serezha.petrov.02@gmail.com>
2022-10-31 13:22:44 +0300

    update TaskPresenter

--- app/javascript/presenters/TaskPresenter.js
+++ app/javascript/presenters/TaskPresenter.js
@@ -1,11 +1,13 @@
 import PropTypes from 'prop-types';
 import PropTypesPresenter from 'utils/PropTypesPresenter';
+import UserPresenter from './UserPresenter';
 
 export default new PropTypesPresenter(
   {
     id: PropTypes.number,
     name: PropTypes.string.isRequired,
     description: PropTypes.string.isRequired,
+    author: UserPresenter.shape(),
   },
   {
     name(task) {
