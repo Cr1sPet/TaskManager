44375ecf2111c6b835108255ca0c3fd495769478
CrsPet <serezha.petrov.02@gmail.com>
2022-11-15 15:26:04 +0300

    created task_updated test in the UserMailerTest

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -17,4 +17,21 @@ class UserMailerTest < ActionMailer::TestCase
     assert email.body.to_s.include?("Task #{task.id} was created")
 
   end
+
+  test "task updated" do
+    user = create(:user)
+    task = create(:task, author: user)
+    params = { user: user, task: task }
+    email = UserMailer.with(params).task_updated
+
+    assert_emails 1 do
+      email.deliver_now
+    end
+
+    assert_equal ['noreply@taskmanager.com'], email.from
+    assert_equal [user.email], email.to
+    assert_equal 'Task Updated', email.subject
+    assert email.body.to_s.include?("Task #{task.id} was updated")
+
+  end
 end
