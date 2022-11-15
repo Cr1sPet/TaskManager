614ab78a0c81657d18be5f750bac3e3ff801f284
CrsPet <serezha.petrov.02@gmail.com>
2022-11-15 16:17:33 +0300

    created task_deleted test

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -34,4 +34,22 @@ class UserMailerTest < ActionMailer::TestCase
     assert email.body.to_s.include?("Task #{task.id} was updated")
 
   end
+
+  test "task deleted" do
+    user = create(:user)
+    task = create(:task, author: user)
+    params = { user: user, task: task }
+    email = UserMailer.with(params).task_deleted
+
+    assert_emails 1 do
+      email.deliver_now
+    end
+
+    assert_equal ['noreply@taskmanager.com'], email.from
+    assert_equal [user.email], email.to
+    assert_equal 'Task Deleted', email.subject
+    assert email.body.to_s.include?("Task #{task.id} was deleted")
+
+  end
+
 end
