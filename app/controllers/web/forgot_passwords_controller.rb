35136b4c6e597248076b0cdd25e8cc1b77359dbc
CrsPet <serezha.petrov.02@gmail.com>
2022-11-16 10:12:24 +0300

    created forgot_password_controller, view and route new for this controller

---
+++ app/controllers/web/forgot_passwords_controller.rb
@@ -0,0 +1,5 @@
+class Web::ForgotPasswordsController < Web::ApplicationController
+  def new
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root :to => "web/boards#show"
 
   scope module: :web do
+    resource :forgot_password, only: :new
     resource :board, only: :show
     resource :session, only: [:new, :create, :destroy]
     resources :developers, only: [:new, :create]
