6534af771ad9f8291b864305b2d04041ad9e7451
CrsPet <serezha.petrov.02@gmail.com>
2022-11-16 19:55:08 +0300

    added token generation and email sent tests to forgot_passwords_controller_test

--- test/controllers/web/forgot_passwords_controller_test.rb
+++ test/controllers/web/forgot_passwords_controller_test.rb
@@ -12,10 +12,18 @@ class Web::ForgotPasswordsControllerTest < ActionController::TestCase
     attrs = {
       email: user.email,
     }
-    post :create, params: { forgot_password_form: attrs }
-
+    assert_emails 1 do
+      post :create, params: { forgot_password_form: attrs }
+    end
     assert_response :success
+
+    user.reload
+
+    assert_not_nil user.recovery_password_token
+    assert_not_nil user.recovery_password_sent_at
+
     assert_match "The link has been sent to the email you entered and will be valid for 24 hours", @response.body
+    assert_match reset_password(user.recovery_password_token), @response.body
 
     attrs = {
       email: "#{user.email}INCORRECT"
