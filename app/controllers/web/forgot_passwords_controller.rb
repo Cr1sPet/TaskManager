458f51da339cd795572d1b9a75da7fce89d511be
CrsPet <serezha.petrov.02@gmail.com>
2022-11-16 14:01:43 +0300

    added create method to forgot_passwords_controller

--- app/controllers/web/forgot_passwords_controller.rb
+++ app/controllers/web/forgot_passwords_controller.rb
@@ -1,5 +1,20 @@
 class Web::ForgotPasswordsController < Web::ApplicationController
   def new
-    
+    @forgot_password = ForgotPasswordForm.new
+  end
+  def create
+
+    @forgot_password = ForgotPasswordForm.new(forgot_password_params)
+
+    if @forgot_password.valid?
+    end
+
+    render(:new)
+
+  end
+
+  private
+  def forgot_password_params
+    params.require(:forgot_password_form).permit(:email)
   end
 end
