2b21abefef0869e1d9f6fa603683a5ffdcc5c49b
CrsPet <serezha.petrov.02@gmail.com>
2022-11-17 00:27:09 +0300

    created test reset_passwords should put update

--- test/controllers/web/reset_passwords_test.rb
+++ test/controllers/web/reset_passwords_test.rb
@@ -7,7 +7,20 @@ class Web::ResetPasswordsControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test 'should post create' do
+  test 'should put update' do
+    user = create(:user)
+
+    token = "token"
+    user.update_column(:recovery_password_token, token)
+    user.update_column(:recovery_password_sent_at, DateTime.now)
+
+
+    attrs = {
+      password: "test_reset_password",
+      password_confirmation: "test_reset_password"
+    }
+    patch :update, params: { id: token, reset_password_form: attrs }
+    assert_response :redirect
 
   end
 
