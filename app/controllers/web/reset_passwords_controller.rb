9e9e6b9d73732aefcc6735807b3423f504669303
CrsPet <serezha.petrov.02@gmail.com>
2022-11-17 16:22:05 +0300

    added tests and  finish reset_password logic

--- app/controllers/web/reset_passwords_controller.rb
+++ app/controllers/web/reset_passwords_controller.rb
@@ -5,6 +5,28 @@ class Web::ResetPasswordsController < Web::ApplicationController
   end
 
   def update
+    @token = params[:id]
+    @reset_password = ResetPasswordForm.new(reset_password_params)
+    @user = User.find_by(recovery_password_token: @token)
+
+    if !@reset_password.valid?
+      render(:edit)
+      return
+    end
+    if !@user || ((Time.now - @user.recovery_password_sent_at) / 1.day) >= 1
+      @incorrect_token = true
+      render(:edit)
+    else
+      @user.password = reset_password_params[:password]
+      @user.recovery_password_token = nil
+      @user.save
+      redirect_to (:root)
+    end
+  end
+
+  private
+  def reset_password_params
+    params.require(:reset_password_form).permit(:password, :password_confirmation)
   end
 
 end
--- test/controllers/web/reset_passwords_test.rb
+++ test/controllers/web/reset_passwords_test.rb
@@ -14,13 +14,15 @@ class Web::ResetPasswordsControllerTest < ActionController::TestCase
     user.update_column(:recovery_password_token, token)
     user.update_column(:recovery_password_sent_at, DateTime.now)
 
-
     attrs = {
       password: "test_reset_password",
       password_confirmation: "test_reset_password"
     }
     patch :update, params: { id: token, reset_password_form: attrs }
     assert_response :redirect
+
+    user.reload
+    assert_nil user.recovery_password_token
 
   end
 
