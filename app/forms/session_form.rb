395e78e6cc5288a5c457c3a9044aafa0dcab6847
CrsPet <serezha.petrov.02@gmail.com>
2022-11-22 13:14:41 +0300

    use standard validation error in session form

--- app/forms/session_form.rb
+++ app/forms/session_form.rb
@@ -18,7 +18,7 @@ class SessionForm
 
   def user_valid?
     if user.blank? || !user.authenticate(password)
-      errors.add(:email, "email or password doesn't match")
+      errors.add(:email, :confirmation, attribute: "email or password")
     end
   end
 end
