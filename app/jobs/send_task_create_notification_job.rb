603cb016226702da22aa67af0d268450f8045326
Cr1sPet <serezha.petrov.02@gmail.com>
2022-12-14 22:19:22 +0300

    use throttled settings in the send_task_create_notification_job

--- app/jobs/send_task_create_notification_job.rb
+++ app/jobs/send_task_create_notification_job.rb
@@ -1,6 +1,6 @@
 class SendTaskCreateNotificationJob < ApplicationJob
   sidekiq_options queue: :mailers
-
+  sidekiq_throttle_as :mailer
   def perform(task_id)
     task = Task.find_by(id: task_id)
     return if task.blank?
