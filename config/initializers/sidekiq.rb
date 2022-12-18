50caa85af07084eaf5d049134cea705dceafcd8b
Cr1sPet <serezha.petrov.02@gmail.com>
2022-12-18 14:09:41 +0300

    updated sidekiq-unique-job config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,15 +2,30 @@
 require 'sidekiq/web'
 require "sidekiq/throttled"
 require "sidekiq/throttled/web"
-require 'sidekiq_unique_jobs/web'
+require "sidekiq-unique-jobs"
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV['REDIS_URL'] }
+  config.redis = { url: ENV["REDIS_URL"] }
+
+  config.client_middleware do |chain|
+    chain.add SidekiqUniqueJobs::Middleware::Client
+  end
+
+  config.server_middleware do |chain|
+    chain.add SidekiqUniqueJobs::Middleware::Server
+  end
+
+  SidekiqUniqueJobs::Server.configure(config)
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV['REDIS_URL'] }
+  config.redis = { url: ENV["REDIS_URL"]}
+
+  config.client_middleware do |chain|
+    chain.add SidekiqUniqueJobs::Middleware::Client
+  end
 end
+
 
 Sidekiq::Throttled::Registry.add(:mailer, { threshold: { limit: 1, period: 5.seconds } })
 
